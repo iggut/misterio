48677811a5ce12aa0302c3a5cb1c93e5d8dd9fb2
IgguT <igor.gutchin@gmail.com>
2023-06-26 12:56:29 -0400

    gs66 starting base

--- hosts/gs66/hardware-configuration.nix
+++ hosts/gs66/hardware-configuration.nix
@@ -1,12 +1,12 @@
 {
   imports = [
-    ../common/optional/ephemeral-btrfs.nix
-    ../common/optional/encrypted-root.nix
+    #../common/optional/ephemeral-btrfs.nix
+    #../common/optional/encrypted-root.nix
   ];
 
   boot = {
     initrd = {
-      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+      availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
       kernelModules = [ "kvm-intel" ];
     };
     loader = {
@@ -18,17 +18,35 @@
     };
   };
 
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/ESP";
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/todo";
       fsType = "vfat";
     };
-  };
 
-  swapDevices = [{
-    device = "/swap/swapfile";
-    size = 8196;
-  }];
+######
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/todo";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/todo";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/disk/by-uuid/todo";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" ];
+      neededForBoot = true;
+    };
+######
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/todo"; }
+    ];
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = true;
